12c5640f4c5e147428ce15b916476835c79b0efa
Thiago <thiagodeboliveira@outlook.com>
2024-09-30 13:16:38 -0300

    Adicionado sistema de login usando localStorage

--- src/router/Cadastro/sect1Cadastro.jsx
+++ src/router/Cadastro/sect1Cadastro.jsx
@@ -1,39 +1,50 @@
 import { DesingSect1Cadastro } from "./styleCadastro";
-import camera from '../../../public/icon-camera.png'
+import camera from '../../../public/icon-camera.png';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
+export default function Sect1Cadastro() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const handleCadastro = () => {
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+        navigate('/login');
+    };
 
-
-export default function Sect1Cadastro(){
-    return(
+    return (
         <DesingSect1Cadastro>
             <div className="cadastro-container">
                 <h1 className="titulo-cadastro">Página de Cadastro</h1>
                 <div className="cadastroprofile-container">
-                    <img 
-                        src={camera} 
-                        alt="" 
-                        className="imagem-cadastro" 
-                    />
-                    <form className="perfil-formcadastro">
-                    
-                    <div className="form-groupcadastro">
-                        
-                        <input type="email" id="email" name="email" placeholder="Email" />
-                    </div>
-                    <div className="form-groupcadastro">
-                        
-                        <input type="password" id="password" name="password" placeholder="Senha"/>
-                    </div>
-                </form>
-                    
-
-                    <button className="btn-cadastro">Entrar</button>
+                    <img src={camera} alt="Ícone de Câmera" className="imagem-cadastro" />
+                    <form className="perfil-formcadastro" onSubmit={(e) => e.preventDefault()}>
+                        <div className="form-groupcadastro">
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-groupcadastro">
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Senha"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                    </form>
+                    <button className="btn-cadastro" onClick={handleCadastro}>Cadastrar</button>
                 </div>
-
-                
-
             </div>
         </DesingSect1Cadastro>
-    )
+    );
 }
--- src/router/Login/StyleLogin.js
+++ src/router/Login/StyleLogin.js
@@ -11,5 +11,73 @@ export const DesignLoginMain = styled.main`
 `
 
 export const DesingSect1Login = styled.section`
+    .login-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        
+    }
+
+    .titulo-login{
+        font-size: 35px;
+        color: #E61631;
+        
+    }
+
+    .loginprofile-container{
+        width: 450px;
+        height: 600px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        border: 2px solid #CF1818;
+        border-radius: 20px;
+        margin-top: 30px;
     
+
+    }
+
+    .imagem-login{
+        border-radius: 100px;
+        background: #C8C8C8;
+        width: 150px;
+        height: 150px;
+        padding: 10px;
+        
+    }
+
+    .perfil-formlogin{
+        display: flex;
+        flex-direction: column;
+        margin-top: 50px;
+       
+        
+        
+    }
+
+    input{
+        width: 300px ;
+        padding: 5px;
+        height: 50px;
+        margin: 10px 5px;
+        border: solid 1px #FB9191;
+        border-radius: 5px;
+    }
+
+    .btn-login{
+        width: 150px;
+        height: 60px;
+        border-radius: 10px;
+        background: #D52B1E;
+        color: white;
+        cursor: pointer;
+        
+    }
+
+    .btn-login:hover{
+        transform: scale(1.1);
+        
+    }
 `
