7fe92bf93ff2be6588e35d8d1075e95ecff86c1b
Ianny Raquel <iannyraquel252488@gmail.com>
2024-10-24 10:20:28 -0300

    estilizando e animando o butão scroll

--- src/router/Inicio/index.jsx
+++ src/router/Inicio/index.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import ImageBanner from "../../../public/inicio-banner.png";
+import { RiScrollToBottomLine } from "react-icons/ri";
 
 const Inicio =()=>{
 
@@ -8,6 +10,47 @@ const Inicio =()=>{
         const content = document.getElementById('content-section');
         content.scrollIntoView({ behavior: 'smooth' });
       };
+
+    // Função da Estilização do Scroll Buttun
+    const StyleScroll = styled.div`
+
+    .scroll-icon-container {
+        position: fixed;
+        bottom: 20px; /* Ajuste conforme necessário */
+        left: 50%;
+        transform: translateX(-50%);
+        animation: fadeInOut 2s infinite; /* Animação de fade in e out */
+    }
+      
+    .scroll-icon {
+        font-size: 50px; /* Ajuste o tamanho do ícone conforme necessário */
+        color: #CF2929FF; /* Cor do ícone */
+        animation: moveDown 2s infinite; /* Animação de movimento */
+        cursor: pointer;
+    }
+      
+    @keyframes moveDown {
+        0% {
+          transform: translateY(0); /* Começo */
+        }
+        50% {
+          transform: translateY(20px); /* Desce */
+        }
+        100% {
+          transform: translateY(0); /* Volta ao início */
+        }
+    }
+      
+    @keyframes fadeInOut {
+        0%, 100% {
+          opacity: 1; /* Visível */
+        }
+        50% {
+          opacity: 0; /* Invisível */
+        }
+    }
+    `
+      
 
     return (
         <>
@@ -25,9 +68,9 @@ const Inicio =()=>{
                     <img src={ImageBanner} alt="image" />
                 </div>
 
-                <div>
-                    <button onClick={scrollToContent}>Scroll to Content</button>
-                </div>
+                <StyleScroll className="scroll-icon-container">
+                    <RiScrollToBottomLine className="scroll-icon" onClick={scrollToContent}/>
+                </StyleScroll>
 
                 <hr className="linha-divisoria"/>
             </section>
