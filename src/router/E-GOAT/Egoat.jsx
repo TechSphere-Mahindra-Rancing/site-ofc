95323727b3d3a808914c6fbfd1e68e9174cef8e5
Jean <jeanmatheus0612@gmail.com>
2024-10-31 15:44:47 -0300

    Estrutura do historico e-goat

--- src/router/E-GOAT/Egoat.jsx
+++ src/router/E-GOAT/Egoat.jsx
@@ -13,6 +13,27 @@ const Egoat =()=>{
             <section className="container">
                 <Top />
             </section>
+
+            <section className="historico">
+                <h2>Histórico</h2>
+                <div>
+                    <h3>01/02/2025</h3>
+                    <p>Compra de Pontos</p>
+                    <p>13:34:12</p>
+                </div>
+                <div>
+                    <h3>27/01/2025</h3>
+                    <p>Apostou em: Jake Dennis</p>
+                    <p>19:53:08</p>
+                    <p>Ganho de 23 pontos</p>
+                    <p>08:00:00</p>
+                </div>
+                <div>
+                    <h3>01/02/2025</h3>
+                    <p>Ganho de 10 pontos</p>
+                    <p>10:29:42</p>
+                </div>
+            </section>
         </MainStyled>
     )
 }
